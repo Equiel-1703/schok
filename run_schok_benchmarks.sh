f24b14b52ffacd603dd70a8adde273c8ea208576
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-01-22 20:09:18 -0300

    Benchmarks values set to definitive values

--- run_schok_benchmarks.sh
+++ run_schok_benchmarks.sh
@@ -25,8 +25,8 @@ echo -e "Runs per benchmark: $RUNS_PER_BENCHMARK\n"
 echo ""  # Add a blank line for readability
 
 # ------------------ Dot Product Benchmark ------------------
-INPUTS="1024 2048 4096" # Test values
-# INPUTS="400000000 500000000 600000000" # CHANGE TO THIS LATER ----------------------------------------------------<<<
+# INPUTS="1024 2048 4096" # Test values
+INPUTS="400000000 500000000 600000000" # CHANGE TO THIS LATER ----------------------------------------------------<<<
 
 echo -e "Dot Product (DP) benchmark\n"
 
@@ -37,8 +37,8 @@ done
 echo ""
 
 # ------------------ Julia Benchmark ------------------
-INPUTS="512 1024 2048" # Test values
-# INPUTS="7168 9216 11264" # CHANGE TO THIS LATER ----------------------------------------------------<<<
+# INPUTS="512 1024 2048" # Test values
+INPUTS="7168 9216 11264" # CHANGE TO THIS LATER ----------------------------------------------------<<<
 
 echo -e "Julia (JL) benchmark\n"
 
@@ -49,8 +49,8 @@ done
 echo ""
 
 # ------------------ MM Benchmarks ------------------
-INPUTS="128 256 512" # Test values
-# INPUTS="5000 7000 9000" # CHANGE TO THIS LATER ----------------------------------------------------<<<
+# INPUTS="128 256 512" # Test values
+INPUTS="5000 7000 9000" # CHANGE TO THIS LATER ----------------------------------------------------<<<
 
 echo -e "Matrix Multiplication (MM) benchmark\n"
 
@@ -61,8 +61,8 @@ done
 echo ""
 
 # ------------------ NBody Benchmarks ------------------
-INPUTS="128 256 512" # Test values
-# INPUTS="100000 200000 400000" # CHANGE TO THIS LATER ----------------------------------------------------<<<
+# INPUTS="128 256 512" # Test values
+INPUTS="100000 200000 400000" # CHANGE TO THIS LATER ----------------------------------------------------<<<
 
 echo -e "nBodies (NB) benchmark\n"
 
@@ -73,8 +73,8 @@ done
 echo ""
 
 # ------------------ Nearest Neighbor Benchmarks ------------------
-INPUTS="1024 2048 4096" # Test values
-# INPUTS="100000000 200000000 300000000" # CHANGE TO THIS LATER ----------------------------------------------------<<<
+# INPUTS="1024 2048 4096" # Test values
+INPUTS="100000000 200000000 300000000" # CHANGE TO THIS LATER ----------------------------------------------------<<<
 
 echo -e "Nearest Neighbor (NN) benchmark\n"
 
@@ -85,8 +85,8 @@ done
 echo ""
 
 # ------------------ Raytracer Benchmarks ------------------
-INPUTS="512 1024 2048" # Test values
-# INPUTS="7168 9216 11264" # CHANGE TO THIS LATER ----------------------------------------------------<<<
+# INPUTS="512 1024 2048" # Test values
+INPUTS="7168 9216 11264" # CHANGE TO THIS LATER ----------------------------------------------------<<<
 
 echo -e "Raytracer (RT) benchmark\n"
 
@@ -97,8 +97,8 @@ done
 echo ""
 
 # ------------------ Saxpy Benchmarks ------------------
-INPUTS="512 1024 2048" # Test values
-# INPUTS="300000000 400000000 500000000" # CHANGE TO THIS LATER ----------------------------------------------------<<<
+# INPUTS="512 1024 2048" # Test values
+INPUTS="300000000 400000000 500000000" # CHANGE TO THIS LATER ----------------------------------------------------<<<
 
 echo -e "Saxpy (SP) benchmark\n"
 
